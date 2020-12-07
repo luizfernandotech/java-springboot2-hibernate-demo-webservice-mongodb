7bc11da4be432797248e6734849bbf5a4fcd2a39
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-12-07 18:36:05 +0000

    Getting a user by id Check list: - In the service.exception subpackage, create ObjectNotFoundException - In UserService, implement the findById method - In UserResource, implement the findById method (return DTO) - In the resources.exception subpackage, create the classes: - - StandardError - - ResourceExceptionHandler

--- src/main/java/com/luizfernandotech/blog/services/UserService.java
+++ src/main/java/com/luizfernandotech/blog/services/UserService.java
@@ -2,10 +2,12 @@ package com.luizfernandotech.blog.services;
 
 import com.luizfernandotech.blog.entities.User;
 import com.luizfernandotech.blog.repository.UserRepository;
+import com.luizfernandotech.blog.services.exeption.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
