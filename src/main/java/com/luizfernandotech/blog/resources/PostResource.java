049e1eada9e167ff7fac2b0e779d13e38b0c697c
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-12-07 19:58:05 +0000

    Getting a post by id Check list: - Create PostService with the findById method - Create PostResource with findById method

---
+++ src/main/java/com/luizfernandotech/blog/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.luizfernandotech.blog.resources;
+
+import com.luizfernandotech.blog.entities.Post;
+import com.luizfernandotech.blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/luizfernandotech/blog/services/PostService.java
@@ -0,0 +1,21 @@
+package com.luizfernandotech.blog.services;
+
+import com.luizfernandotech.blog.entities.Post;
+import com.luizfernandotech.blog.repository.PostRepository;
+import com.luizfernandotech.blog.services.exeption.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
