4beeebb5bf622b4b30af0279a3b9e17f5e9991fc
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-12-07 19:43:52 +0000

    Projection of author data with DTO Check list: - Create AuthorDTO - Refactor Post - Refactor the initial database load - - IMPORTANT: now it is necessary to persist User objects before relating

--- src/main/java/com/luizfernandotech/blog/config/Instantiation.java
+++ src/main/java/com/luizfernandotech/blog/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.luizfernandotech.blog.config;
 
+import com.luizfernandotech.blog.dto.AuthorDTO;
 import com.luizfernandotech.blog.entities.Post;
 import com.luizfernandotech.blog.entities.User;
 import com.luizfernandotech.blog.repository.PostRepository;
@@ -30,10 +31,11 @@ public class Instantiation implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com");
         User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post p1 = new Post(null, Instant.parse("2020-06-20T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-        Post p2 = new Post(null, Instant.parse("2020-06-20T19:53:07Z"), "Bom dia", "Acordei feliz hoje!", u1);
-
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+        Post p1 = new Post(null, Instant.parse("2020-06-20T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+        Post p2 = new Post(null, Instant.parse("2020-06-20T19:53:07Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
